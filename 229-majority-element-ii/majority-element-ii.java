07b00f8174b14bd9afa23283a18851ba5bfa20f0
Himanshu Gupta <jaiswalhimanshu664455@gmail.com>
2026-09-10 12:41:47 +0530

    Time: 2 ms (99.95%) | Memory: 52.4 MB (65.03%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
         int p=0;
@@ -14,5 +14,64 @@ class Solution {
         }
      }
        return list;     
+    }
+}
+*/
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        int candidate1 = 0;
+        int candidate2 = 0;
+
+        int count1 = 0;
+        int count2 = 0;
+
+        // Step 1: Find two candidates
+        for (int num : nums) {
+
+            if (num == candidate1) {
+                count1++;
+            }
+            else if (num == candidate2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            }
+            else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        if (count1 > nums.length / 3) {
+            ans.add(candidate1);
+        }
+
+        if (count2 > nums.length / 3) {
+            ans.add(candidate2);
+        }
+
+        return ans;
     }
 }
