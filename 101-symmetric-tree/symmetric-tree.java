1857e0f376ba0e134b38271c055b32f13bee939d
Himanshu Gupta <jaiswalhimanshu664455@gmail.com>
2026-09-14 01:14:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (96.51%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return check(root.left, root.right);
+    }
+
+    public boolean check(TreeNode left, TreeNode right) {
+
+        // Both are null
+        if (left == null && right == null) {
+            return true;
+        }
+
+        // One is null
+        if (left == null || right == null) {
+            return false;
+        }
+
+        // Values are different
+        if (left.val != right.val) {
+            return false;
+        }
+
+        // Mirror comparison
+        return check(left.left, right.right) &&
+               check(left.right, right.left);
+    }
+}
